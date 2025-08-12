e6f081684e41bfa5f3e2ba21336747747eb85bfe
MuriloSouza955 <souza.murilo@gmail.com>
2025-08-12 17:46:17 -0300

    feat: shake feedback ehwn user makes a wrong guess

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,7 @@ export default function App() {
   const [letter, setLetter] = useState("");
   const [lettersUsed, setLettersUsed] = useState<LettersUsedProps[]>([]);
   const [challenge, setChallenge] = useState<Challenge | null>(null);
+  const [shake, setShake] = useState(false)
 
   function handlerRestartGame() {
     const isConfirmed = window.confirm("Você tem certeza que deseja reiniciar?")
@@ -68,6 +69,11 @@ export default function App() {
     setLettersUsed((prevState) => [...prevState, { value, correct }]);
     setScore(currentScore);
     setLetter("");
+
+    if(!correct){
+      setShake(true)
+      setTimeout(() => setShake(false), 300)
+    }
   }
 
   function endGame(message: string) {
@@ -109,7 +115,7 @@ export default function App() {
         />
         <Tip tip={challenge.tip} />
 
-        <div className={styles.word}>
+        <div className={`${styles.word} ${shake && styles.shake}`}>
           {challenge.word.split("").map((letter, index) => {
             const letterUsed = lettersUsed.find(
               (used) => used.value.toUpperCase() === letter.toUpperCase()
